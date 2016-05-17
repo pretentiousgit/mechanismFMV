e5553daa193410c86af1ef16cc129c1b949cd0ba
Alex Leitch <alex.leitch@gmail.com>
2016-05-17 08:16:38 -0400

    feat(Readme): Updates to readme, some comment clearing

--- README.md
+++ README.md
@@ -4,11 +4,19 @@ This is a display face for the screenPerfect (http://www.github.com/pretentiousg
 
 ### Documentation
 
-Mechanism is intended to be a complete project, which means that it is a mess intended to display privately.
+Mechanism is a repo at the end of time! It's built to display on a private server with content available to people connected to that server. It is based on work from Eyebeam NYC in 2013 - the idea that people behave differently in private is a pretty strong one, I like game arcades that don't require huge amounts of technology to be supplied by the gallery, etc.
+
+### What It Does
+This loads a list of videos from place A into memory on a client device, place B. It also loads a touch layer over those videos, making any of them qualify as a type of FMV experience. When the videos have loaded, the touch layer lets you select between them using a socket server.
+
+### Improvements I'd Make Now
+
+- Actually stream video from server
+- a more clever video load system - at the moment, this tries to stream an unlimited amount of video on page load. It works best with small videos and/or GIF/Vinealikes.
 
 ### Contributing
 
-Please go to http://www.github.com/jennie/iv and contribute to that instead. It's much more solid than this, which is basically a chat client modified to serve linked video files in private.
+This is a repo at the end of time! It's basically the supporting code for a thesis project, designed in concert with OCADu's game::play lab in 2014. This is the most finished version of it.
 
 ### Possible Directions
 
--- screenperfect.js
+++ screenperfect.js
@@ -3,10 +3,10 @@ var express = require('express'),
     path = require('path'),
     wrench = require('wrench'),
     exec = require('child_process').exec,
-    util = require('util'),    
+    util = require('util'),
     fs = require('fs'),
     eavesdropper = 0,
-    Files = {}, 
+    Files = {},
     _hopper,
     _game,
     json, app = express.createServer();
@@ -15,11 +15,13 @@ require('sugar')
 require('ejs')
 
 //express setup
-app.configure(function () {
+app.configure(function() {
     app.set('views', __dirname);
-    app.set('view engine', 'ejs');    
+    app.set('view engine', 'ejs');
     app.use(express.bodyParser());
-    app.use(express.logger('\033[90m:date :method :url :response-time\\ms\033[0m \033[31m:referrer \033[0m'));
+    app.use(express.logger(
+        '\033[90m:date :method :url :response-time\\ms\033[0m \033[31m:referrer \033[0m'
+    ));
     app.use(express.methodOverride());
     app.use(app.router);
     app.use(express.errorHandler({
@@ -29,43 +31,37 @@ app.configure(function () {
     app.use(express.static(__dirname + '/public'));
 });
 
-
-// load and pass JSON to front end to load the videos properly
-// var gamedata = require(__dirname + '/public/tmp/gamedata.json');
-// app.locals.gamefile = JSON.stringify(gamedata);
-
-
-app.get('/', function (req, res, next) {
-    // res.locals.gamedata = req.gamedata
+app.get('/', function(req, res, next) {
     res.render('_start');
 });
 
-app.get('/control', function (req, res, next) {
+app.get('/control', function(req, res, next) {
     res.render('control');
 });
 
-app.get('/setup', function (req, res, next) {
+app.get('/setup', function(req, res, next) {
     res.render('setup');
 });
 
-app.get('/client', function (req, res, next) {
+app.get('/client', function(req, res, next) {
     res.render('client');
 });
 
 //socket stuff
 var io = sio.listen(app),
     control = false,
-    listener; 
+    listener;
 
 //start server
-app.listen(3003, function () {
+app.listen(3003, function() {
     var addr = app.address();
-    console.log('   app is listening on\033[31m http://' + addr.address + ':' + addr.port +'\033[0m');
+    console.log('   app is listening on\033[31m http://' + addr.address + ':' +
+        addr.port + '\033[0m');
 });
 
 function handler(req, res) {
     fs.readFile(__dirname + '/index.html',
-        function (err, data) {
+        function(err, data) {
             if (err) {
                 res.writeHead(500);
                 return res.end('Error loading index.html');
@@ -75,61 +71,63 @@ function handler(req, res) {
         });
 }
 
-io.sockets.on('connection', function (socket) { 
+io.sockets.on('connection', function(socket) {
     //send various events to connected sockets
-				
-        socket.on('control event', function (e) {
-            socket.broadcast.emit('control event', e);
-            console.log('control event ' + e);
-            _hopper = e;
-            return false;
-        });
+    socket.on('control event', function(e) {
+        socket.broadcast.emit('control event', e);
+        console.log('control event ' + e);
+        _hopper = e;
+        return false;
+    });
 
-        socket.on('setup event', function (e) {
-            socket.broadcast.emit('setup event', e);
-            console.log(e);
+    socket.on('setup event', function(e) {
+        socket.broadcast.emit('setup event', e);
+        console.log(e);
 
-            var nameOfFile = e[0]; // this needs to be stringified and parsed to get appropriate file id
-            fs.writeFile('tmp/' + nameOfFile + ".json", JSON.stringify(e), function (err) {
+        var nameOfFile = e[0]; // this needs to be stringified and parsed to get appropriate file id
+        fs.writeFile('tmp/' + nameOfFile + ".json", JSON.stringify(e),
+            function(err) {
                 if (err) {
                     console.log(err);
                 } else {
                     console.log(nameOfFile + " Divlist was saved!");
                 }
             });
-        });
+    });
 
-		socket.on('listener', function (listener, e) {
-			eavesdropper++;
-			console.log('eavesdropper '+ eavesdropper+' connected, _hopper is '+ _hopper);			
-		});
+    socket.on('listener', function(listener, e) {
+        eavesdropper++;
+        console.log('eavesdropper ' + eavesdropper +
+            ' connected, _hopper is ' + _hopper);
+    });
 
-        socket.on('load', function(e){
-            console.log('load' + e);
-            socket.broadcast.emit('load', e);
-            _game = e;
-            return false;
-        });
+    socket.on('load', function(e) {
+        console.log('load' + e);
+        socket.broadcast.emit('load', e);
+        _game = e;
+        return false;
+    });
 
-        socket.on('role', function (role, func) {
-            console.log('socket assuming role ' + role); 
-            if (control){
-             	func(true);
-            }
-			else {
-                control = socket.control = true;
-                socket.broadcast.emit('announcement', 'control connected');
-                func(false);
-            }
-            
-        });
+    socket.on('role', function(role, func) {
+        console.log('socket assuming role ' + role);
+        if (control) {
+            func(true);
+        } else {
+            control = socket.control = true;
+            socket.broadcast.emit('announcement', 'control connected');
+            func(false);
+        }
 
-        //if the control disconnects then tell the clients.
-        socket.on('disconnect', function () {
-            if (!socket.control) { 
-                socket.broadcast.emit('announcement', 'eavesdropper has disconnected');                
-                return};
-            control = false;
-            socket.broadcast.emit('disconnect', 'control disconnected');
-        });
+    });
+
+    //if the control disconnects then tell the clients.
+    socket.on('disconnect', function() {
+        if (!socket.control) {
+            socket.broadcast.emit('announcement',
+                'eavesdropper has disconnected');
+            return
+        };
+        control = false;
+        socket.broadcast.emit('disconnect', 'control disconnected');
+    });
 });
